8af380875e2316063756e9771cd4ccde5249de33
Jonathan Villegas <jvillegas86@gmail.com>
2025-10-27 23:56:19 -0300

    working on last week activity

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,47 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        PromptUserName();
+        PromptUserNumber();
+
+    }
+
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program");
+    }
+
+    static string PromptUserName()
+    {
+        Console.WriteLine("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.WriteLine("Please enter your favorite number: ");
+        string userNumber = Console.ReadLine();
+        int favoriteNumber = int.Parse(userNumber);
+        return favoriteNumber;
+
+    }
+
+    static int SquareNumber(int myNumber)
+    {
+        double squaredNum = Math.Pow(myNumber, 2);
+        int intNum = (int)squaredNum;
+        return intNum;
+    }
+    
+    static void DisplayResult()
+    {
+        
     }
 }
