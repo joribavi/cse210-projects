12d57ed6e3ffeecc92abecf25f3405e871fd3817
Jonathan Villegas <jvillegas86@gmail.com>
2025-10-28 22:07:37 -0300

    -m sending changes: activity 1, week02

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        Resume myResume = new Resume();
+        myResume._name = "John Doe";
+        
+        Job job1 = new Job();
+
+        job1._company = "Waltmart";
+        job1._jobTitle = "Customer Service";
+        job1._startYear = 1995;
+        job1._endYear = 2003;
+
+        Job job2 = new Job();
+        job2._company = "Mc-Donnals";
+        job2._jobTitle = "Customer Service";
+        job2._startYear = 2003;
+        job2._endYear = 2013;
+
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.DisplayResumeDetails();
+
+     // Console.WriteLine($"{myResume._jobs[0]._jobTitle}"); testing acces to first job Title 
+
+
+
     }
 }
