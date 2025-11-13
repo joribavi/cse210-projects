79293f4f6dc376fc158494d5037fac6de087e9b2
Jonathan Villegas <jvillegas86@gmail.com>
2025-11-12 23:46:01 -0300

    continue to work on the very basis of the program

--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -1,9 +1,29 @@
+using System.Runtime.CompilerServices;
+
 public class Scripture
 {
     private string _reference;
     private List<Word>Words; 
 
-    //Constructors
-  //  public Scripture()
+    public Scripture(Reference reference, string text)
+    {
+        
+    }
 
+
+    public void HideRandWords(int numberToHide)
+    {
+        
+    }
+
+    public string GetDisplayText()
+    {
+        return _reference + Words; 
+    }
+
+    public bool IsCompletelyHidden()
+    {
+      return true;   
+    
+    }
 }
