b59ceaddcc6bf9e976aebcf46ddffb05b5ee6333
Jonathan Villegas <jvillegas86@gmail.com>
2025-11-21 17:15:35 -0300

    erasing some comments in the code

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -79,13 +79,4 @@ class Program
     }
 
 
-    /*  PROGRAM SPECIFICATION 
-P
-
-
- write a program that creates at least two orders with a 2-3 products each.
-  Call the methods to get the packing label, the shipping label, and the total price of the order, 
-  and display the results of these methods.
-
-     */
 }
